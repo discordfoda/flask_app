f857a34873046981b359e3d81f60ff59410b0308
discordfoda <151239782+discordfoda@users.noreply.github.com>
2023-11-20 17:01:47 -0300

    Update GenNitro.py

--- GenNitro.py
+++ GenNitro.py
@@ -1,39 +1,46 @@
-import random
-import string
 import requests
 from threading import Thread
 import time 
+import itertools
+import string
 
-
-def GenNitro():
-    nitro = "https://discord.gift/{}".format(String())
-    url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+def GenNitro(sstr):
+    nitro = "https://discord.gift/{}".format(sstr)
+    url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{sstr}?with_application=false&with_subscription_plan=true"
     response = requests.get(url) 
     if response.status_code == 200: 
-        print(f"Successful || {nitro}")
+        print(f"Successful || {nitro}") 
         exit()
     else:
         print(f'Error || {nitro}')
 
-def String():
-    letras = string.ascii_letters + string.digits
-    return ''.join(random.choice(letras) for _ in range(16))
 
 
+def generate_combinations(length=16):
+    characters = string.ascii_letters + string.digits
+    for combination in itertools.product(characters, repeat=length):
+        yield ''.join(combination)
 
+combinations_generator = generate_combinations()
+print('''
+        Made By Wirilless
+            
+        GenNitro V1
+        ''')
+time.sleep(1)
+input("Pressione Enter para continuar...")
 
-try:
-    print('''
-    Made By Wirilless
-          
-    GenNitro V1
-    ''')
-    time.sleep(1)
-    
-    while True:  
-        processo = Thread(target=GenNitro())
+while True:
+    try:
+        
+
+        current_combination = next(combinations_generator)
+        processo = Thread(target=GenNitro(current_combination))
         processo.start()
+        processo.join()
 
-except KeyboardInterrupt:
-    print("KeyBoardInterrupt")
-    exit()
+    except KeyboardInterrupt:
+        print("KeyBoardInterrupt")
+        exit()
+        
+        
